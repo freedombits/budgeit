6708fbf3e102b469a16c0add96f790e84dac6810
sblmnl <39979686+sblmnl@users.noreply.github.com>
2022-07-13 15:08:50 -0400

    Add JWT bearer authentication

--- server/src/Api/Program.cs
+++ server/src/Api/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.Audience = builder.Configuration.GetValue<string>("Jwt:Audience");
+            options.Authority = builder.Configuration.GetValue<string>("Jwt:Authority");
+            options.RequireHttpsMetadata = builder.Environment.IsProduction();
+        });
+}
 
 var app = builder.Build();
 {
