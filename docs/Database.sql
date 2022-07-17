9e4682e61526d97f05d39b40b484edb14e9ddbe6
sblmnl <39979686+sblmnl@users.noreply.github.com>
2022-07-17 12:38:38 -0400

    Drop redundant table

--- docs/Database.sql
+++ docs/Database.sql
@@ -1,43 +1,36 @@
-CREATE TABLE budgets (
+CREATE TABLE users (
     id CHAR(36) NOT NULL,
-    currency VARCHAR(8) NOT NULL,
+    currency VARCHAR(4) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE budgets_categories (
+CREATE TABLE users_categories (
     id CHAR(36) NOT NULL,
     name VARCHAR(128) NOT NULL,
-    budget_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (budget_id) REFERENCES (budgets.id)
+    FOREIGN KEY (user_id) REFERENCES (users.id)
 );
 
-CREATE TABLE budgets_categories_allocations (
+CREATE TABLE users_categories_allocations (
     id CHAR(36) NOT NULL,
-    amount DECIMAL(65, 2) NOT NULL,
+    amount DECIMAL NOT NULL,
     timestamp DATETIME NOT NULL,
-    budget_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
     category_id CHAR(36) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (budget_id) REFERENCES (budgets.id),
+    FOREIGN KEY (user_id) REFERENCES (users.id),
     FOREIGN KEY (category_id) REFERENCES (budgets_categories.id)
 );
 
-CREATE TABLE budgets_categories_expenses (
+CREATE TABLE users_categories_expenses (
     id CHAR(36) NOT NULL,
-    amount DECIMAL(65, 2) NOT NULL,
+    amount DECIMAL NOT NULL,
     note VARCHAR(128) NOT NULL,
     timestamp DATETIME NOT NULL,
-    budget_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
     category_id CHAR(36) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (budget_id) REFERENCES (budgets.id),
+    FOREIGN KEY (user_id) REFERENCES (users.id),
     FOREIGN KEY (category_id) REFERENCES (budgets_categories.id)
-);
-
-CREATE TABLE users (
-    id CHAR(36) NOT NULL,
-    budget_id CHAR(36) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (budget_id) REFERENCES (budgets.id)
 );
